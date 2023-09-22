2ab18a1349f05bbb5973912352b4d3f3c4871702
shitiantian05 <shitiantian05@meituan.com>
2023-09-22 14:16:21 +0800

    分类训练测试

--- mytrain.py
+++ mytrain.py
@@ -1,7 +1,13 @@
+"""
+prepare
+modifiy ultralytics/cfg/datasets/*.yaml for your own dataset
+modify ultralytics/cfg/model/v8/yolov8-cls.yaml , nc: number of classes
+"""
+
 from ultralytics import YOLO
 from importlib.metadata import PackageNotFoundError, version
 # Load a model
 model = YOLO('/Users/shitiantian05/sttCodes/test0818/ultralytics/yolov8n-cls.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data='/Users/shitiantian05/sttCodes/test0818/ultralytics/mydataset', epochs=100, imgsz=640, device='mps')
+results = model.train(data='/Users/shitiantian05/sttCodes/test0818/ultralytics/mydataset', epochs=20, imgsz=640, device='mps')
